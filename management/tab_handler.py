a1054dac2c946bfb2da6274e073c95fad7c827fc
tiblazy <hauntedapply@gmail.com>
2022-09-20 11:24:00 -0300

    fix: refatoração de calculo parte 02

--- management/tab_handler.py
+++ management/tab_handler.py
@@ -1,12 +1,18 @@
+import re
+from utils.json_handler import read_json
 from datetime import datetime
 
+DATABASE_PATH = "menu.json"
+
 def calculate_tab(table: dict):    
-    total = {}
-    for item in table:
-        for amount in item.keys():
-            total[amount] = total.get(amount, 0) + item[amount]
-    subtotal = total["amount"]
-    
+    database = read_json(DATABASE_PATH)
+
+    subtotal = 0
+    for product in table:
+        for item in database:
+            if product["id"] == item["id"]:
+                subtotal += item["price"] * product["amount"]
+                
     local_format = "%d/%m/%y %H:%M:%S"
     date_now = datetime.now()
     created_at = date_now.strftime(local_format)        
